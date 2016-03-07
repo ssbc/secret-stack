11c6aa465ad5a594b45555f5c3c4ba24c4133770
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-08 12:00:31 +1300

    test setting timeouts

---
+++ test/timeout.js
@@ -0,0 +1,65 @@
+var Illuminati = require('../')
+var crypto = require('crypto')
+var tape = require('tape')
+var seeds = require('./seeds')
+
+//deterministic keys make testing easy.
+function hash (s) {
+  return crypto.createHash('sha256').update(s).digest()
+}
+
+var appkey = hash('test_key')
+
+var create = Illuminati({
+  appKey: appkey,
+}).use({
+  manifest: {
+    hello: 'sync'
+  },
+  permissions: {
+    anonymous: { allow: ['hello'], deny: null }
+  },
+  init: function (api) {
+    return {
+      hello: function (name) {
+        return 'Hello, ' + name + '.'
+      }
+    }
+  }
+})
+
+var alice = create({ seed: seeds.alice, timeout: 100, defaultTimeout: 5e3 })
+var carol = create({ seed: seeds.alice, timeout: 0, defaultTimeout: 10 })
+var bob = create({ seed: seeds.bob })
+
+tape('alice connects to bob', function (t) {
+
+  var connected = false, disconnected = false
+
+  alice.connect(bob.address(), function (err, rpc) {
+    if(err) throw err
+    var start = Date.now()
+    rpc.on('closed', function () {
+      console.log('time to close:', Date.now() - start)
+      t.ok(connected)
+      t.notOk(disconnected)
+      t.end()
+    })
+  })
+
+  carol.connect(bob.address(), function (err, rpc) {
+    connected = true
+    carol.on('closed', function (t) {
+      disconnected = true
+    })
+  })
+
+})
+
+tape('cleanup', function (t) {
+  alice.close(true); bob.close(true); carol.close(true)
+  t.end()
+})
+
+
+
