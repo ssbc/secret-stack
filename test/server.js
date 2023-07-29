251fd145be5b8f07c19a315beed2a7ac85ae4318
André Staltz <andre@staltz.com>
2023-07-29 12:12:42 +0300

    support bare mode: no net, no shs (#87)

--- test/server.js
+++ test/server.js
@@ -1,6 +1,6 @@
 var tape = require('tape')
 var crypto = require('crypto')
-var SecretStack = require('../lib')
+var SecretStack = require('../lib/bare')
 var seeds = require('./seeds')
 
 // deterministic keys make testing easy.
@@ -8,25 +8,26 @@ function hash (s) {
   return crypto.createHash('sha256').update(s).digest()
 }
 
-var appkey = hash('test_key')
+var appKey = hash('test_key')
 
-var create = SecretStack({
-  appKey: appkey
-}).use({
-  manifest: {
-    hello: 'sync'
-  },
-  permissions: {
-    anonymous: { allow: ['hello'], deny: null }
-  },
-  init: function (api) {
-    return {
-      hello: function (name) {
-        return 'Hello, ' + name + '.'
+var create = SecretStack({ appKey })
+  .use(require('../lib/plugins/net'))
+  .use(require('../lib/plugins/shs'))
+  .use({
+    manifest: {
+      hello: 'sync'
+    },
+    permissions: {
+      anonymous: { allow: ['hello'], deny: null }
+    },
+    init: function (api) {
+      return {
+        hello: function (name) {
+          return 'Hello, ' + name + '.'
+        }
       }
     }
-  }
-})
+  })
 
 var alice = create({ seed: seeds.alice })
 var bob = create({ seed: seeds.bob })
